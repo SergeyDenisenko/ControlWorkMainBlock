36b4f31948aaa18e1b478ad7172c97c6b80ec895
Sergey <denisenko.sa@ffx.media>
2024-02-29 08:33:44 +0300

    Закоментировал строку проверки

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def get_array_strings(max_char):
     return [item for item in input("Введите строку: ").split() if len(item) <= max_char]
 
-print(get_array_strings(3))
+# print(get_array_strings(3))
